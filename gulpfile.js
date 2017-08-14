b5e13205009b969aed277ef347d3bc033c3bb0a4
José Cage <josejanuario7@gmail.com>
2017-08-14 22:34:27 +0100

    Added gulp-imagemin to minify images and gulp-connect webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,41 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var imagemin = require('gulp-imagemin');
 
 //style paths
 var ficheirosSass = 'src/sass/**/*.scss',
     destino = 'assets/css/';
 
+// Start a development webserver.
+gulp.task('webserver', function() {
+	connect.server({
+		livereload: true,
+		root: '.'
+	});
+});
+
 gulp.task('styles', function(){
     gulp.src(ficheirosSass)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(destino));
+    	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest(destino))
+        .pipe(connect.reload());
+});
+
+gulp.task('imagemin', function() {
+	gulp.src('src/img/**/*{.png,.jpg,.jpeg,.gif,.svg}')
+	.pipe(imagemin({
+		interlaced: true,
+		progressive: true,
+		optimizationLevel: 5
+	}))
+	.pipe(gulp.dest('assets/img/'))
 });
 
 gulp.task('watch',function() {
-    gulp.watch(ficheirosSass,['styles']);
+	gulp.watch(ficheirosSass,['styles'])
+	gulp.watch('src/img/**', ['imagemin'])
+    connect.reload();
 });
+
+gulp.task('default', ['styles', 'imagemin', 'webserver', 'watch']);
